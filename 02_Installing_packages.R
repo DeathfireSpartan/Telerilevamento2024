abb3ef86733d701d73d5de8b07185d94e2a61c08
DeathfireSpartan <162352313+DeathfireSpartan@users.noreply.github.com>
2024-03-07 17:49:09 +0100

    Create 02_Installing_packages.R

---
+++ 02_Installing_packages.R
@@ -0,0 +1,10 @@
+# Installing new packages in R
+# Installazione del pacchetto Terra
+
+# Funzione per installare pacchetti --> install.packages("nomepacchetto")
+# Va già a pigliare il pacchetto da CRAN --> fa scegliere il server da cui scaricare il pacchetto
+install.packages("terra")
+
+# Funzione library --> controlla che il pacchetto sia stato installato correttamente
+library(terra)
+# Funzione remove.packages("terra")
