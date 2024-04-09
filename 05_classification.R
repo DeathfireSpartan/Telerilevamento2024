c7d6fec01736c6c42d8bc269c29a84a3930216da
DeathfireSpartan <162352313+DeathfireSpartan@users.noreply.github.com>
2024-04-09 17:42:07 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -91,3 +91,4 @@ p1 <- ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identi
 p2 <- ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1 + p2
 
+
