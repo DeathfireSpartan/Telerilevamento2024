5eef2c9a0f2675d5e579117e90bacd241753afc7
DeathfireSpartan <162352313+DeathfireSpartan@users.noreply.github.com>
2024-04-09 17:39:32 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -39,49 +39,54 @@ m2006c <- im.classify(m2006, num_clusters=2)
 # classe 1 = foresta
 # classe 2 = umani
 
-# Frequenzefi
+# Frequenze 1992
 f1992 <- freq(m1992c)
 
-# proportions
+# Proporzioni 1992 = numero di pixel di una classe rispetto al totale
 tot1992 <- ncell(m1992c)
 prop1992 = f1992 / tot1992
 
-# percentages
+# Percentuali 1992
 perc1992 = prop1992 * 100
 
-# 17% human, 83% forest
+# Foresta = 83%
+# Umani = 17%
 
-# frequencies
+# Frequenze 2006
 f2006 <- freq(m2006c)
 
-# proportions
+# Proporzioni 2006
 tot2006 <- ncell(m2006c)
 prop2006 = f2006 / tot2006
 
-# percentages
+# Percentuali 2006
 perc2006 = prop2006 * 100
 
-# 1992: 17% human, 83% forest
-# 2006: 55% human, 45% forest
+# 1992: 17% umani, 83% foresta
+# 2006: 55% umani, 45% foresta
 
-# let's build a dataframe
-class <- c("forest", "human")
+# Costruisco un dataframe (tabella) con le 2 classi e i rispettivi valori
+class <- c("Foresta", "Umani") # Uso le virgolette perchè è un testo
 p1992 <- c(83, 17)
 p2006 <- c(45, 55)
 
+# Funzione data.frame(classi, dato1, dato2, ...)
 tabout <- data.frame(class, p1992, p2006)
-tabout
+View(tabout) # Visualizzo la tabella
 
-# plotting the output
+# Plotto il grafico usando il pacchetto ggplot2
+# Funzione ggplot(dati, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white")
+# aes --> estetica --> come costruire il grafico
+# geom_bar --> tipo di geometria, in questo caso istogramma --> gli argomenti della funzione indicano le statistiche e il colore
 ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white")
 ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white")
 
-# patchwork
+# Uso il pacchetto patchwork --> posso mettere i grafici a confronto
 p1 <- ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white")
 p2 <- ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white")
 p1 + p2
 
-# varying axis and using lines
+# Funzione ylim() per eguagliare gli assi dei due grafici
 p1 <- ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p2 <- ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1 + p2
